896860f59f03cc722f61388281a83c121c65f71d
affggh <879632264@qq.com>
2025-01-17 22:20:42 +0800

    ui: Support good look on windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 import 'package:mtk_garbage_porttool/pages/pages.dart';
 
@@ -7,6 +12,10 @@ const double _kDefaultTitleBarHeight = 36;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Window.initialize();
+  if (Platform.isWindows) {
+    await Window.hideWindowControls();
+  }
   windowManager.ensureInitialized();
 
   WindowOptions windowOptions = const WindowOptions(
@@ -18,12 +27,47 @@ Future<void> main() async {
     titleBarStyle: TitleBarStyle.hidden,
     windowButtonVisibility: false,
   );
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
+  await windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await Window.setEffect(
+      effect: await getCorrectWindowEffect(),
+      color: Platform.isWindows ? const Color(0xCC222222) : Colors.transparent,
+      dark: true,
+    );
     await windowManager.show();
     await windowManager.focus();
   });
 
+
   runApp(const MyApp());
+}
+
+
+Future<int> getWindowsBuildNumber() async {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  WindowsDeviceInfo windowsInfo = await deviceInfo.windowsInfo;
+
+  // 获取系统版本
+  int buildNumber = windowsInfo.buildNumber;
+
+  return buildNumber;
+}
+
+Future<WindowEffect> getCorrectWindowEffect() async {
+  if (Platform.isWindows) {
+    final buildNumber = await getWindowsBuildNumber();
+    if (buildNumber >= 22523) {
+      return WindowEffect.tabbed;
+    } else if (buildNumber >= 22000) {
+      return WindowEffect.mica;
+    } else
+      if (buildNumber >= 10240) {
+        return WindowEffect.acrylic;
+    } else {
+      return WindowEffect.solid;
+    }
+  } else {
+    return WindowEffect.solid;
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -41,8 +85,15 @@ class _MyAppState extends State<MyApp> {
     return FluentApp(
       home: const MyNav(),
       debugShowCheckedModeBanner: false,
-      theme: FluentThemeData.light(),
-      darkTheme: FluentThemeData.dark(),
+      theme: FluentThemeData.light().copyWith(
+          scaffoldBackgroundColor: Colors.transparent,
+          navigationPaneTheme:
+              NavigationPaneThemeData(backgroundColor:Platform.isWindows ? Colors.transparent : null)),
+      darkTheme: FluentThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Colors.transparent,
+          navigationPaneTheme:
+              NavigationPaneThemeData(backgroundColor:Platform.isWindows ? Colors.transparent : null)),
+      color: Colors.transparent,
       themeMode: themeMode,
     );
   }
@@ -70,25 +121,30 @@ class _MyNavState extends State<MyNav> {
   Widget build(BuildContext context) {
     return NavigationView(
       appBar: NavigationAppBar(
-        leading: Image.asset("assets/CircleCashTeamLogo.png", width: 24,height: 24,),
+          backgroundColor: Colors.transparent,
+          leading: Image.asset(
+            "assets/CircleCashTeamLogo.png",
+            width: 24,
+            height: 24,
+          ),
           automaticallyImplyLeading: false,
           height: _kDefaultTitleBarHeight,
           actions: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Tooltip(
-                message: "切换亮暗主题",
-                child: IconButton(
-                    icon: Icon(
-                        FluentTheme.of(context).brightness == Brightness.dark
-                            ? FluentIcons.lower_brightness
-                            : FluentIcons.brightness),
-                    onPressed: () {
-                      context
-                          .findAncestorStateOfType<_MyAppState>()
-                          ?.toggleTheme(context);
-                    }),
-              ),
+              //Tooltip(
+              //  message: "切换亮暗主题",
+              //  child: IconButton(
+              //      icon: Icon(
+              //          FluentTheme.of(context).brightness == Brightness.dark
+              //              ? FluentIcons.lower_brightness
+              //              : FluentIcons.brightness),
+              //      onPressed: () {
+              //        context
+              //            .findAncestorStateOfType<_MyAppState>()
+              //            ?.toggleTheme(context);
+              //      }),
+              //),
               WindowCaptionButton.minimize(
                 brightness: FluentTheme.of(context).brightness,
                 onPressed: () => windowManager.minimize(),
@@ -115,23 +171,25 @@ class _MyNavState extends State<MyNav> {
           },
           items: [
             PaneItem(
-                icon: const Icon(FluentIcons.home),
-                title: const Text("主页"),
-                body: PageHome(),),
+              icon: const Icon(FluentIcons.home),
+              title: const Text("主页"),
+              body: PageHome(),
+            ),
             PaneItem(
-                icon: const Icon(FluentIcons.app_icon_default),
-                title: const Text("移植"),
-                body: PagePort(),),
+              icon: const Icon(FluentIcons.app_icon_default),
+              title: const Text("移植"),
+              body: PagePort(),
+            ),
           ],
           footerItems: [
             PaneItem(
                 icon: const Icon(FluentIcons.info),
                 body: PageInfo(),
                 title: const Text("关于")),
-            PaneItem(
-                icon: const Icon(FluentIcons.settings),
-                body: const Center(child: Text("Settings")),
-                title: const Text("设置"))
+            //PaneItem(
+            //    icon: const Icon(FluentIcons.settings),
+            //    body: const Center(child: Text("Settings")),
+            //    title: const Text("设置"))
           ]),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import macos_window_utils
 import package_info_plus
 import screen_retriever_macos
 import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
